1c397ea3672f5f13e1a8f590a237ad76c70d3e27
Marija Kuveljic <kuveljic117@gmail.com>
2024-06-27 22:20:25 +0200

    Import PageGenerator and PostForm in App.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,67 @@
 import './styles.css';
 import React from 'react';
 
-import { Landing } from '@homework-task/components/landing/Landing';
+import {
+    PageGenerator,
+    Section,
+} from '@homework-task/components/PageGenerator';
+import PostForm from '@homework-task/components/PostForm';
+
+const sections: Section[] = [
+    {
+        type: 'layoutSection',
+        props: { background: 'bg-white', children: null },
+        components: [
+            {
+                type: 'componentHero',
+                props: {
+                    title: 'Welcome to the Hero Section',
+                    image: '/media/landing/hero.svg',
+                },
+            },
+        ],
+    },
+    {
+        type: 'layoutSection',
+        props: { background: 'bg-gray-100', children: null },
+        components: [
+            {
+                type: 'componentItemsShowcase',
+                props: {
+                    items: [
+                        {
+                            title: 'Item 1',
+                            description: 'Description 1',
+                        },
+                        {
+                            title: 'Item 2',
+                            description: 'Description 2',
+                        },
+                    ],
+                },
+            },
+            {
+                type: 'componentTrustBar',
+                props: {
+                    images: [
+                        '/media/cats/cat_1.png',
+                        '/media/cats/cat_2.png',
+                        '/media/cats/cat_3.png',
+                        '/media/cats/cat_4.png',
+                        '/media/cats/cat_5.png',
+                        '/media/cats/cat_6.png',
+                    ],
+                },
+            },
+        ],
+    },
+];
 
 const App: React.FC = () => {
     return (
         <main>
-            <Landing />
+            <PostForm />
+            <PageGenerator sections={sections} />
         </main>
     );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import App from './App';
+
+const queryClient = new QueryClient();
 
 const container = document.getElementById('root');
 
 if (container) {
     createRoot(container).render(
         <React.StrictMode>
-            <App />
+            <QueryClientProvider client={queryClient}>
+                <App />
+            </QueryClientProvider>
         </React.StrictMode>
     );
 }
